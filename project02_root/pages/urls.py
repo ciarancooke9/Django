474262ca03481220ada908a62fad7d1f80dc0a3b
Ciaran Cooke <ciaran.cooke9@mail.dcu.ie>
2021-04-17 15:58:23 +0000

    pages working

--- project02_root/pages/urls.py
+++ project02_root/pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('<str:pagename>/', views.index, name='index'),
     path('', views.index, name='index'),
 ]
--- project02_root/pages/views.py
+++ project02_root/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from . models import Page
 
-def index(request):
-    return HttpResponse("Project02 Site")
+def index(request, pagename=''):
+	pagename = '/' + pagename
+	pg = Page.objects.get(permalink=pagename)
+	return HttpResponse(pg.bodytext)
 
 # Create your views here.
