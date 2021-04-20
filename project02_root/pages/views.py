b315c933d7bc6ede0bc77e0c6bbb3de0d55828b4
Kevin McGettigan <kevin.mcgettigan3@mail.dcu.ie>
2021-04-20 17:15:36 +0000

    Staging Changes

--- project02_root/pages/views.py
+++ project02_root/pages/views.py
@@ -6,7 +6,13 @@ from . models import Show, Genre, Actor
 def index(request, pagename=''):
 	pagename = '/' + pagename
 	pg = Page.objects.get(permalink=pagename)
+	num_actor = Actor.objects.all().count()
+	num_shows = Show.objects.all().count()
+	num_genres = Genre.objects.all().count()
 	context = {
+		'num_actor': num_actor,
+		'num_shows': num_shows,
+		'num_genres': num_genres,
 		'title': pg.title,
 		'context': pg.bodytext, # note the end-of-line comma
 		'last_updated': pg.update_date,
